10d53c26d013164b0b503ff6af0189aa78051fac
SOYEONG OH <axionoso@gmail.com>
2019-03-07 16:27:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,7 +2,7 @@ function basicTeenager(age) {
   if (13 <= age && age <= 19) {
     return "You are a teenager!";
   } else {
-    
+
   }
 }
 
@@ -30,9 +30,27 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age){
-    case "age >= 13 && age <= 19":
+    case "13":
       console.log("You are a teenager")
       break;
+      case "14":
+        console.log("You are a teenager")
+        break;
+        case "15":
+          console.log("You are a teenager")
+          break;
+          case "16":
+            console.log("You are a teenager")
+            break;
+            case "17":
+              console.log("You are a teenager")
+              break;
+              case "18":
+                console.log("You are a teenager")
+                break;
+                case "19":
+                  console.log("You are a teenager")
+                  break;
     default:
       console.log("You have an age")
   }
